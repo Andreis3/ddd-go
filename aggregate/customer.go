d670f5376dfffa0b6db96acfd1dea41d71e649a4
Andreis3 <andrei.as3@hotmail.com>
2022-02-21 22:36:15 -0300

    Aggregates — Combined Entities and Value Objects

---
+++ aggregate/customer.go
@@ -0,0 +1,19 @@
+// Package aggregates holds aggregates that combines many entities into a full object
+
+package aggregate
+
+import (
+	"github.com/santos/ddd-go/entity"
+	"github.com/santos/ddd-go/valueobject"
+)
+
+// Customer is a aggregate that combines all entities needed to represent a customer
+type Customer struct {
+	// person is the root entity of a customer
+	// which means the person.ID is the main identifier for this aggregate
+	person *entity.Person
+	// a customer can hold many products
+	products []*entity.Item
+	// a customer can perform many transactions
+	transactions []valueobject.Transaction
+}
