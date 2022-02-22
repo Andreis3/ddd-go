d59a09365482c4002321a4b21205794ec5503529
Andreis3 <andrei.as3@hotmail.com>
2022-02-21 23:07:59 -0300

    Factories — Encapsulate complex logic

--- aggregate/customer.go
+++ aggregate/customer.go
@@ -3,8 +3,17 @@
 package aggregate
 
 import (
+	"errors"
+
+	"github.com/google/uuid"
+
 	"github.com/santos/ddd-go/entity"
 	"github.com/santos/ddd-go/valueobject"
+)
+
+var (
+	// ErrInvalidPerson is returned when the person is not valid in the NewCustomer factory
+	ErrInvalidPerson = errors.New("a customer has to have an valid person")
 )
 
 // Customer is a aggregate that combines all entities needed to represent a customer
@@ -16,4 +25,26 @@ type Customer struct {
 	products []*entity.Item
 	// a customer can perform many transactions
 	transactions []valueobject.Transaction
+}
+
+// NewCustomer is a factory to create a new Customer aggregate
+// It will validate that the name is not empty
+func NewCustomer(name string) (Customer, error) {
+	// Validate that the Name is not empty
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+	person := &entity.Person{
+		Name: name,
+		ID:   uuid.New(),
+	}
+
+	// Create a customer object and initialize all the values to avoid nil pointer exceptions
+	customer := Customer{
+		person:       person,
+		products:     make([]*entity.Item, 0),
+		transactions: make([]valueobject.Transaction, 0),
+	}
+
+	return customer, nil
 }
